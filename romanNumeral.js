8076d3bbb8977a24c08f1d2795c21ec6fc9583ea
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-10-03 18:27:41 -0300

    Create romanNumeral.js

---
+++ romanNumeral.js
@@ -0,0 +1,68 @@
+function convertToRoman(num) {
+  let romanNumeral = []
+  if (num > 3999) {
+    return "The highest number in Roman Numerals is 3999, please try another number."
+  }
+  if (num > 999) {
+    do {
+      num -= 1000;
+      romanNumeral.push("M")
+    } while (num > 999) 
+  }
+  if (num > 899) {
+    num -= 900;
+    romanNumeral.push("CM");
+  }
+  if (num > 499) {
+    num -= 500;
+    romanNumeral.push("D")
+  }
+  if (num > 399) {
+    num -= 400;
+    romanNumeral.push("CD");
+  }
+  while (num > 99) {
+  num -= 100;
+  romanNumeral.push("C")
+  }
+  if (num > 89) {
+    num -= 90;
+    romanNumeral.push("XC")
+  }
+  if (num > 49) {
+    num -= 50;
+    romanNumeral.push("L")
+  }
+  if (num > 39) {
+    num -= 40;
+    romanNumeral.push("XL");
+  }
+  while (num > 9) {
+    num -= 10;
+    romanNumeral.push("X");
+  }
+   while (num > 99) {
+  num -= 100;
+  romanNumeral.push("C")
+  }
+  if (num > 8) {
+    num -= 9;
+    romanNumeral.push("IX")
+  }
+  if (num > 4) {
+    num -= 5;
+    romanNumeral.push("V")
+  }
+  if (num > 3) {
+    num -= 4;
+    romanNumeral.push("IV");
+  }
+  while (num > 0) {
+    num -= 1;
+    romanNumeral.push("I");
+  }
+
+ romanNumeral = romanNumeral.join("");
+ console.log(romanNumeral);
+ return romanNumeral;
+}
